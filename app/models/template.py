942578145d41c3cb4e9a79b60c2c41e1fed88028
stefanmuhlack <147387990+stefanmuhlack@users.noreply.github.com>
2024-07-17 16:58:30 +0200

    Added TemplateField model to manage fields within templates.

--- app/models/template.py
+++ app/models/template.py
@@ -22,3 +22,13 @@ class TemplateField(Base):
     height = Column(Integer, nullable=False)
     template_id = Column(Integer, ForeignKey('templates.id'))
     template = relationship('Template', back_populates='fields')
+    __tablename__ = 'template_fields'
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String(255), nullable=False)
+    field_type = Column(String(50), nullable=False)
+    x = Column(Integer, nullable=False)
+    y = Column(Integer, nullable=False)
+    width = Column(Integer, nullable=False)
+    height = Column(Integer, nullable=False)
+    template_id = Column(Integer, ForeignKey('templates.id'))
+    template = relationship('Template', back_populates='fields')
